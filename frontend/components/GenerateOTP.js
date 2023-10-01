f2619128d7e7bcfe9ea1eb8819f8cc679c74fbd1
Sshovon <frcshovon@gmail.com>
2023-10-02 02:09:47 +0600

    frontend env

--- frontend/components/GenerateOTP.js
+++ frontend/components/GenerateOTP.js
@@ -11,10 +11,13 @@ const GenerateOTP = () => {
 
   const generateOTP = async () => {
     try {
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/generate-otp`, {
-        email,
-      });
-      console.log(response)
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/generate-otp`,
+        {
+          email,
+        }
+      );
+      console.log(response);
       setOTP(response.data.otp);
       setResponse('OTP generated successfully.');
     } catch (error) {
@@ -24,23 +27,30 @@ const GenerateOTP = () => {
   };
 
   return (
-    <div>
-      <h2>Generate OTP</h2>
-      <div>
-        <label>Email:</label>
+    <div className="p-4 bg-gray-100">
+      <h2 className="text-2xl font-bold mb-4">Generate OTP</h2>
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-700">Email:</label>
         <input
+          className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:border-blue-300"
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
       </div>
       <div>
-        <button onClick={generateOTP}>Generate OTP</button>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:bg-blue-600"
+          onClick={generateOTP}
+        >
+          Generate OTP
+        </button>
       </div>
       {otp && (
-        <div>
-          <p>Generated OTP: {otp}</p>
-          <p>{response}</p>
+        <div className="mt-4">
+          <p className="font-semibold">Generated OTP:</p>
+          <p className="text-xl">{otp}</p>
+          <p className="mt-2">{response}</p>
         </div>
       )}
     </div>
--- frontend/components/VerifyOTP.js
+++ frontend/components/VerifyOTP.js
@@ -1,22 +1,23 @@
-// components/VerifyOTP.js'
+// components/VerifyOTP.js
 "use client"
-
-
 import React, { useState } from 'react';
 import axios from 'axios';
 
 const VerifyOTP = () => {
-    console.log(process.env.API_URL)
+  console.log(process.env.NEXT_PUBLIC_API_URL);
   const [email, setEmail] = useState('');
   const [otp, setOTP] = useState('');
   const [response, setResponse] = useState('');
 
   const verifyOTP = async () => {
     try {
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/verify-otp`, {
-        email,
-        otp,
-      });
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/verify-otp`,
+        {
+          email,
+          otp,
+        }
+      );
       setResponse('OTP verified successfully.');
     } catch (error) {
       console.error('Error verifying OTP:', error);
@@ -25,28 +26,35 @@ const VerifyOTP = () => {
   };
 
   return (
-    <div>
-      <h2>Verify OTP</h2>
-      <div>
-        <label>Email:</label>
+    <div className="p-4 bg-gray-100">
+      <h2 className="text-2xl font-bold mb-4">Verify OTP</h2>
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-700">Email:</label>
         <input
+          className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:border-blue-300"
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
       </div>
-      <div>
-        <label>OTP:</label>
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-700">OTP:</label>
         <input
+          className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:border-blue-300"
           type="text"
           value={otp}
           onChange={(e) => setOTP(e.target.value)}
         />
       </div>
       <div>
-        <button onClick={verifyOTP}>Verify OTP</button>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:bg-blue-600"
+          onClick={verifyOTP}
+        >
+          Verify OTP
+        </button>
       </div>
-      <p>{response}</p>
+      <p className="mt-4 text-red-600">{response}</p>
     </div>
   );
 };
